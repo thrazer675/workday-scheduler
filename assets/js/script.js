b9268505a7dc26756761082be06d70031bcd9067
thrazer675 <thrazer675@aol.com>
2021-01-31 17:19:57 -0500

    added time classes for input boxes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,23 @@
 var today = moment().format("dddd, MMM Do YYYY");
+var time = moment().format("H");
+var events = JSON.parse(localStorage.getItem("taskObject")) || [];
+
 
 $(window).on("load", function() {
 
     $("#currentDay").text(today);
-})
+
+    $(".input-block").each(function() {
+        var hour = parseInt($(this).prev().attr("data-hour"));
+
+        if (hour < time) {
+            $(this).addClass("past");
+        } else if (hour > time) {
+            $(this).addClass("future");
+        } else {
+            $(this).addClass("present");
+        }
+
+    });
+
+});
